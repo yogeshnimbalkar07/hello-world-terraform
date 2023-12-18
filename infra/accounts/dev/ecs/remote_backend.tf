d30b69f3947cd671a020c4de1163c9056dbd07ef
Sagar Maurya <147434812+asmigar@users.noreply.github.com>
2023-12-18 21:37:01 +0530

    change backend config

--- infra/accounts/dev/ecs/remote_backend.tf
+++ infra/accounts/dev/ecs/remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "asmigar-ello-world-terraform-state"
+    bucket         = "asmigar-hello-world-terraform-state"
     key            = "dev/ecs/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "hello-world-state-locks"
