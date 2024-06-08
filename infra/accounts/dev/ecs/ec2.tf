65bbab48fea76b687777ad4baeff39c72c862821
yogeshnimbalkar07 <162571678+yogeshnimbalkar07@users.noreply.github.com>
2024-06-08 20:17:57 +0530

    Update ec2.tf

--- infra/accounts/dev/ecs/ec2.tf
+++ infra/accounts/dev/ecs/ec2.tf
@@ -86,7 +86,7 @@ resource "aws_autoscaling_group" "ecs" {
 }
 
 resource "aws_ecs_capacity_provider" "main" {
-  name = "demo-ecs-ec2"
+  name = "demo1-ecs-ec2"
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.ecs.arn
