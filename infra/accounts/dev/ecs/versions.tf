dba53e5e95c203d856b01d01506ec76c31fdd5cc
yogeshnimbalkar07 <162571678+yogeshnimbalkar07@users.noreply.github.com>
2024-06-08 19:10:17 +0530

    Update versions.tf

--- infra/accounts/dev/ecs/versions.tf
+++ infra/accounts/dev/ecs/versions.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      version = "= 5.47.0"
+      version = "= 5.53.0"
       source  = "hashicorp/aws"
     }
   }
-  required_version = "= 1.5.7"
+  required_version = "= 1.8.4"
 }
